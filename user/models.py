1ea6755d65e07bba5fb74a8fdfc774d9f04a999a
ogabek <quvvatullayev@gmail.com>
2023-09-14 19:24:10 +0500

    creat user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class AuthUser(AbstractBaseUser):
+    name = models.CharField(max_length=30)
+    tg_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return self.name
