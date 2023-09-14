ea1130d0c2234f2c66079236869281c3c18f65de
ogabek <quvvatullayev@gmail.com>
2023-09-14 21:12:46 +0500

    creat adn fix usr app

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import AuthUser, Order, Like
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 
-admin.site.unregister([Group])
+admin.site.unregister([Group, User])
 admin.site.register([AuthUser, Order, Like])
--- user/models.py
+++ user/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import User
 from product.models import Product
 
-class AuthUser(AbstractBaseUser):
+class AuthUser(User):
     name = models.CharField(max_length=30)
     tg_name = models.CharField(max_length=100)
-    email = models.EmailField()
     phone = models.PositiveIntegerField()
 
     def __str__(self) -> str:
