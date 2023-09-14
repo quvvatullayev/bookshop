9d9d9da7b14c0166ea9f1c625bd087251a513d51
ogabek <quvvatullayev@gmail.com>
2023-09-14 20:53:59 +0500

    add unregister Groups

--- user/admin.py
+++ user/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import AuthUser, Order, Like
+from django.contrib.auth.models import Group
 
+admin.site.unregister([Group])
 admin.site.register([AuthUser, Order, Like])
