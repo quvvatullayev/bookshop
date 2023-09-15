b7cc85c514a3bdfbaec734d2c5eabba5321eed0a
ogabek <quvvatullayev@gmail.com>
2023-09-15 14:43:27 +0500

    add urls advertisement views

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views.category import CategoryListCreateView, CategoryRetrieveUpdateDestroyView
 from .views.product import ProductListCreateView, ProductRetrieveUpdateDestroyView
+from .views.advertisement import AdvertisementListCreateView, AdvertisementRetrieveUpdateDestroy
 
 app_name = 'category'
 urlpatterns = [
@@ -8,4 +9,6 @@ urlpatterns = [
     path('category/<int:pk>/', CategoryRetrieveUpdateDestroyView.as_view()),
     path('product/', ProductListCreateView.as_view()),
     path('product/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view()),
+    path('advertisement/', AdvertisementListCreateView.as_view()),
+    path('advertisement/<int:pk>/', AdvertisementRetrieveUpdateDestroy.as_view()),
 ]
