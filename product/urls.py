ff0ad1659fe6ab2b658e23b8a5cc83dfa95efefb
ogabek <quvvatullayev@gmail.com>
2023-09-15 09:13:12 +0500

    create add product views urls

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views.category import CategoryListCreateView, CategoryRetrieveUpdateDestroyView
+from .views.product import ProductListCreateView, ProductRetrieveUpdateDestroyView
 
 app_name = 'category'
 urlpatterns = [
     path('category/', CategoryListCreateView.as_view()),
-    path('category/<int:pk>/', CategoryRetrieveUpdateDestroyView.as_view())
+    path('category/<int:pk>/', CategoryRetrieveUpdateDestroyView.as_view()),
+    path('product/', ProductListCreateView.as_view()),
+    path('product/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view()),
 ]
