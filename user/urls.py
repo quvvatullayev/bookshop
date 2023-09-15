5bf2b29c21206356688415e7357bb23d0f0b82bc
ogabek <quvvatullayev@gmail.com>
2023-09-15 09:40:56 +0500

    add urls like views

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views.authuser import AuthUserListCreateView, AuthUserRetrieveUpdateDestroyView
 from .views.order import OrderListCreateView, OrderRetrieveUpdateDestroy
+from .views.like import LikeListCreateView, LikeRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('order/', OrderListCreateView.as_view()),
     path('order/<int:pk>/', OrderRetrieveUpdateDestroy.as_view()),
+    path('like/', LikeListCreateView.as_view()),
+    path('like/<int:pk>/', LikeRetrieveUpdateDestroyView.as_view()),
 ]
