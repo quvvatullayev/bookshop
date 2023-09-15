29ff1a6e43a7a01b0032900a235cae7f5a7afe3d
ogabek <quvvatullayev@gmail.com>
2023-09-15 11:33:51 +0500

    base url add img setting

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,8 @@ from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -26,4 +28,4 @@ urlpatterns = [
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('product.urls')),
     path('auth/', include('user.urls')),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
