d23e38107c78007f477a5140e7e3694bccfed32d
ogabek <quvvatullayev@gmail.com>
2023-09-15 09:11:27 +0500

    create product retrieve updeate destroy view

--- product/views/product.py
+++ product/views/product.py
@@ -2,7 +2,10 @@ from rest_framework import generics
 from ..models import Product 
 from ..serialization import ProductSerializer
 
-class ProductListView(generics.ListCreateAPIView):
+class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     permission_classes = ProductSerializer
-    
+
+class ProductRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
