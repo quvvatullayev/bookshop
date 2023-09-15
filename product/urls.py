14e08fa891a6c133eb73eaff8f534043731bc9a9
ogabek <quvvatullayev@gmail.com>
2023-09-15 15:05:53 +0500

    fix retrieve advertisement view

--- product/urls.py
+++ product/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('category/', CategoryListCreateView.as_view()),
     path('category/<int:pk>/', CategoryRetrieveUpdateDestroyView.as_view()),
     path('product/', ProductListCreateView.as_view()),
-    path('product/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view()),
+    path('product/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view(), name='product'),
     path('advertisement/', AdvertisementListCreateView.as_view()),
     path('advertisement/<int:pk>/', AdvertisementRetrieveUpdateDestroy.as_view()),
 ]
--- product/views/advertisement.py
+++ product/views/advertisement.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
 from ..models import Advertisement
-from ..serialization import AdvertisementSerializer
+from ..serialization import AdvertisementSerializer 
+from ..models import Product
+from ..serialization import ProductSerializer
+from rest_framework.reverse import reverse, reverse_lazy
+from rest_framework.response import Response
 
 class AdvertisementListCreateView(generics.ListCreateAPIView):
     queryset = Advertisement.objects.all()
@@ -9,3 +13,12 @@ class AdvertisementListCreateView(generics.ListCreateAPIView):
 class AdvertisementRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
+    
+    def retrieve(self, request, *args, **kwargs):
+        advertisement_obj = Advertisement.objects.get(id = kwargs['pk'])
+        advertisement = AdvertisementSerializer(advertisement_obj, many = False).data
+
+        product_obj = Product.objects.get(id = advertisement['product'])
+        product = ProductSerializer(product_obj, many = False).data
+
+        return Response(reverse_lazy(request=request, viewname='category:product', kwargs={'pk':product['id']}))
