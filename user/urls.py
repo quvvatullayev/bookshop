54942cab9d09890740589dcd3745a4a85ad42f00
ogabek <quvvatullayev@gmail.com>
2023-09-15 09:34:12 +0500

    add urls Order views

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views.authuser import AuthUserListCreateView, AuthUserRetrieveUpdateDestroyView
+from .views.order import OrderListCreateView, OrderRetrieveUpdateDestroy
 
 urlpatterns = [
+    path('order/', OrderListCreateView.as_view()),
+    path('order/<int:pk>/', OrderRetrieveUpdateDestroy.as_view()),
 ]
