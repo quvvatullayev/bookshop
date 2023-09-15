4ca8b940bf9479ec1fafb156fb30f9121b75797c
ogabek <quvvatullayev@gmail.com>
2023-09-15 15:58:55 +0500

    add urls author book views

--- product/urls.py
+++ product/urls.py
@@ -2,13 +2,20 @@ from django.urls import path
 from .views.category import CategoryListCreateView, CategoryRetrieveUpdateDestroyView
 from .views.product import ProductListCreateView, ProductRetrieveUpdateDestroyView
 from .views.advertisement import AdvertisementListCreateView, AdvertisementRetrieveUpdateDestroy
+from .views.authorbooks import AuthorBooksListCreateView, AuthorBookRetrieveUpdateDestroyView
+
 
 app_name = 'category'
 urlpatterns = [
     path('category/', CategoryListCreateView.as_view()),
     path('category/<int:pk>/', CategoryRetrieveUpdateDestroyView.as_view()),
+
     path('product/', ProductListCreateView.as_view()),
     path('product/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view(), name='product'),
+
     path('advertisement/', AdvertisementListCreateView.as_view()),
     path('advertisement/<int:pk>/', AdvertisementRetrieveUpdateDestroy.as_view()),
+
+    path('authorbook/', AuthorBooksListCreateView.as_view()),
+    path('authorbook/<int:pk>/', AuthorBookRetrieveUpdateDestroyView.as_view()),
 ]
