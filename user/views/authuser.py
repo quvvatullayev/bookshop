a662f6c4dd0afbf5be8c00f9a76debb601d222bc
ogabek <quvvatullayev@gmail.com>
2023-09-22 09:12:54 +0500

    create auth user read only permission class

--- user/views/authuser.py
+++ user/views/authuser.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from ..models import AuthUser
 from ..serialization import AuthUserSerializer
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
 
 class AuthUserListCreateView(generics.ListCreateAPIView):
     queryset = AuthUser.objects.all()
@@ -9,3 +11,7 @@ class AuthUserListCreateView(generics.ListCreateAPIView):
 class AuthUserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = AuthUser.objects.all()
     serializer_class = AuthUserSerializer
+
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
