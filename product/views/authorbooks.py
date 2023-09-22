3736d0c8c966a3e76878d94e81a9d717abf14d32
ogabek <quvvatullayev@gmail.com>
2023-09-22 09:35:41 +0500

    add auth author books classes

--- product/views/authorbooks.py
+++ product/views/authorbooks.py
@@ -2,14 +2,18 @@ from rest_framework import generics
 from ..models import AuthorBook, Product
 from ..serialization import AuthorBooksSerializer, ProductSerializer
 from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser
+from user.views.authuser import ReadOnly
 
 class AuthorBooksListCreateView(generics.ListCreateAPIView):
     queryset = AuthorBook.objects.all()
     serializer_class = AuthorBooksSerializer
+    permission_classes = [IsAdminUser|ReadOnly]
 
 class AuthorBookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = AuthorBook
     serializer_class = AuthorBooksSerializer
+    permission_classes = [IsAdminUser|ReadOnly]
 
     def retrieve(self, request, *args, **kwargs):
         authorbook_obj = AuthorBook.objects.get(id = kwargs['pk'])
